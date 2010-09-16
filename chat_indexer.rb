5bfa7c6786c3c4e2ee0c4b7bfb29f4c4f4d324de
Mikiya Okuno <mikiya.okuno@gmail.com>
2010-09-16 09:25:35 +0900

    fixed license terms

--- chat_indexer.rb
+++ chat_indexer.rb
@@ -4,13 +4,14 @@
 #
 #Author::       Mikiya Okuno
 #Version::      1.0
-#License::      GPLv3
+#License::      GPLv2 or later
 #Copyright::    Copyright (c) 2010, Mikiya Okuno. All rights reserved.
 
 #--
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License.
+# the Free Software Foundation, either version 2 of the License, or
+# any later version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
